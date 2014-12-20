d35470a134f93330bb917fd06adcc0ff27b5db4e
Allight7 <allight.me@ya.ru>
2014-12-20 21:55:08 +0300

    Added code comments

---
+++ munro_paterson.h
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <algorithm> 
+#include <exception> 
+
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+
+inline double log2(double n){
+	return log(n) / log(2);
+}
+
+//выибирает каждый второй элемент из отсортированного объединения двух векторов
+
+inline void merge(std::vector<int> & v1, std::vector<int> &v2, int s){ 
+	int j = 0, k = 0;
+	std::vector<int> res(s);
+	forn(i, s){
+		k >=s || (j < s && v1[j] < v2[k]) ? j++ : k++;
+		res[i] = k >=s || (j < s && v1[j] < v2[k]) ? v1[j++] : v2[k++];
+	}
+	v1 = res;
+	return;
+};
+
+/*
+	int* a - исходная последовательсть 
+	int an - длина последовательности 
+	int p - число проходов по полседовательности
+	int rank - ранг искомого элемента в исходной последовательсти
+	
+	return - искомый элемент с рангом rank
+*/
+
+inline int munro_paterson(int* a, int an, int p, int rank){
+	assert(a && an > 0);						//непустая последовательность
+	assert(p > 0);								//ненулевое кол-во проходов
+	assert(rank >= 0 && rank < an);				//валидное значение ранга
+	if(an == 1) return *a;
+	
+	
+	float m = an;
+	float c = 2.;	//const	addition								
+	int lower = INT_MIN;
+	int upper = INT_MAX;
+	
+
+	float first_predicted_space = pow(m, 1./p) * c * pow(log2(m), 2. - 2./p);	//оценка максимума памяти для алгоритма в 1/sizeof(int).
+
+	forn(i,p-1){																
+		if(m <= first_predicted_space) break;									
+		float space = pow(m, 1./(p-i)) * c * pow(log2(m), 2. - 2./(p-i));		
+		int s = floor(space/log2(m));											//округление вверх. охват всей последовательности,	
+		int t = ceil(log2(m/s));												//требуется заполнение фиктивн. элем.
+		int m_taken = pow(2,t)*s;												//разм. расшир. послед.
+		int t_2 = pow(2,t);
+		int rank_offset = 0;
+		std::vector<int> v_curr(s);												//текущий сэмпл
+		std::vector<bool> b_per_level(t+1, false);								//наличие сохраненных сэмплов для конкр. уровня.
+		std::vector<std::vector<int>> v_per_level(t+1);							//контейнер с сэмплами разных уровней, ожидающих слияния
+		forn(i,t+1)
+			v_per_level[i].resize(s);											
+
+		for(int curr = 0, taken = 0; taken < m_taken;){							/*=================================================*/
+			if(curr < an){														
+				forn(j,s){
+					while(curr < an && (a[curr] < lower || a[curr] > upper)){
+						if(a[curr] < lower) ++rank_offset;
+						++curr;
+					}
+					v_curr[j] = curr < an ? a[curr++] : INT_MAX;
+				}
+				taken += s;
+				sort(v_curr.begin(), v_curr.end());												/*	Сэмплирование	*/
+			}
+			else{
+				v_curr = std::vector<int>(s, INT_MAX);			
+				taken += s;
+			}
+			int level = 0;
+			while(b_per_level[level]){
+				merge(v_curr, v_per_level[level], s);
+				b_per_level[level++] = 0;
+			}
+			v_per_level[level] = v_curr;
+			b_per_level[level] = 1;
+		}																		/*=================================================*/
+
+		forn(i,t)																	//проверка корректности сэмплирования:
+			assert(!b_per_level[i]);												//все уровни, кроме последнего должны быть пусты
+		assert(b_per_level[t]);
+		
+		v_curr = v_per_level[t];
+
+		int l = floor(static_cast<float>(rank - rank_offset) / (t_2)) - t;			//расчет индексов новых границ внутри t-сэмпла
+		int u = ceil(static_cast<float>(rank - rank_offset) / (t_2));
+
+		if(l >= 0 && l < s && v_curr[l] < upper && v_curr[l] > lower) lower = v_curr[l];	//обновление границ с проверкой корректности
+		if(u >= 0 && u < s && v_curr[u] > lower && v_curr[u] < upper) upper = v_curr[u];		
+
+		int lbound = t_2 * (l);
+		int ubound = t_2 * (u + t);													//обновление максимально возможного числа элементов
+		m = ubound - lbound > 0 ? ubound - lbound : 1;								//для следующего прохода
+	}
+																				/*================================================*/
+																							/*	Последний проход	*/
+	assert(first_predicted_space >= m);												//проверка ограничения используемой памяти
+
+	int rank_offset = 0;
+	std::vector<int> res;
+	for(int curr = 0; curr < an;){
+		while(curr < an && (a[curr] < lower || a[curr] > upper)){					//отсев элементов; увеличение оффсэта ранга, 
+			if(a[curr] < lower) ++rank_offset;										//если эл-т не проходит по левой границе
+			++curr;
+		}
+		if(curr >= an)
+			break;
+		res.push_back(a[curr++]);
+	}
+
+	sort(res.begin(), res.end());
+
+	assert(rank - rank_offset >= 0);												//проверка корректрости рассчета оффсета ранга
+	return res[rank-rank_offset];
+}
--- test_wa.cpp
+++ test_wa.cpp
@@ -1,40 +1,35 @@
-#include "munro_peterson.h"
+#include "munro_paterson.h"
 #include <iostream>
 #include <ctime>
-#include <chrono>
-#include <random>
 
 const int MAX_SIZE = 10000;
 int a[MAX_SIZE];
 int b[MAX_SIZE];
 
 int main(){
-	int n[] = {1, 3, 10, MAX_SIZE}; //запуск на различных объемах данных
+	int n[] = {1, 3, 10, MAX_SIZE}; //Р·Р°РїСѓСЃРє РЅР° СЂР°Р·Р»РёС‡РЅС‹С… РѕР±СЉРµРјР°С… РґР°РЅРЅС‹С…
 	int nn = 4;
-	int p[] = {1, 2, 4, 6, 20};		//запуск c различным числом проходов
+	int p[] = {1, 2, 4, 6, 20};		//Р·Р°РїСѓСЃРє c СЂР°Р·Р»РёС‡РЅС‹Рј С‡РёСЃР»РѕРј РїСЂРѕС…РѕРґРѕРІ
 	int pn = 5;
 	
-	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::default_random_engine generator (seed);
-	std::uniform_int_distribution<int> distribution(0,INT_MAX-1);
-
+	srand(time(nullptr));
+	
 	forn(q,100){
 		forn(i,nn){
 			forn(j,n[i]){
-				a[j] = distribution(generator);
+				a[j] = rand() % INT_MAX;
 				b[j] = a[j];
 			}
 			std::sort(b,b+n[i]);		
-			int ass = 12;
 			forn(k,pn){
-				int rank = 0;					//запуск c различным числом проходов
-				assert(b[rank] == munro_peterson(a, n[i], p[k], rank));
+				int rank = 0;					//Р·Р°РїСѓСЃРє c СЂР°Р·Р»РёС‡РЅС‹Рј С‡РёСЃР»РѕРј РїСЂРѕС…РѕРґРѕРІ
+				assert(b[rank] == munro_paterson(a, n[i], p[k], rank));
 			
 				rank = rand() % n[i];
-				assert(b[rank] == munro_peterson(a, n[i], p[k], rank));
+				assert(b[rank] == munro_paterson(a, n[i], p[k], rank));
 			
 				rank = n[i] - 1;
-				assert(b[rank] == munro_peterson(a, n[i], p[k], rank));
+				assert(b[rank] == munro_paterson(a, n[i], p[k], rank));
 			}		
 		}
 	}
